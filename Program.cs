89888cf697c7b41c783b76023f5941b3a44442d4
BuzganVlada <156216745+blizzfemonema@users.noreply.github.com>
2024-07-12 18:51:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class Student
+{
+    public string Nume { get; set; }
+    public int Varsta { get; set; }
+    public string Specializare { get; set; }
+
+    public Student(string nume, int varsta, string specializare)
+    {
+        Nume = nume;
+        Varsta = varsta;
+        Specializare = specializare;
+    }
+    
+    public Student(Student copie)
+    {
+        Nume = copie.Nume;
+        Varsta = copie.Varsta;
+        Specializare = copie.Specializare;
+    }
+
+    public void AfisareDetalii()
+    {
+        Console.WriteLine($"Nume: {Nume}");
+        Console.WriteLine($"Varsta: {Varsta}");
+        Console.WriteLine($"Specializare: {Specializare}");
+    }
+}
